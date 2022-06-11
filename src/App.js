84b34836fcf4a5b98b0b9550e87df63a1ad4480d
minhazpro3 <minhazpro1@gmail.com>
2022-06-11 18:45:24 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
     </div>
   );
 }
 
 export default App;
+
+
+// step 1 : install packages
+// step 2 : constant define
+// step 3 : async action create
+// step 4 : reducers
+// step 5 : create store
+// step 6 : provide store
+// step 7 : use store 
+// step 8 : adding store
